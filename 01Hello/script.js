69006fc642582ce5d9b6f6301b3668408efc2c62
Ravi Yadav <74930003+ryadavtmc@users.noreply.github.com>
2021-04-10 10:46:43 -0500

    Added my project

---
+++ 01Hello/script.js
@@ -0,0 +1,57 @@
+console.log("Hello Raj");
+
+var txt = "abcdefghijklm";
+var pos = txt.
+indexOf("h")
+;
+console.log(pos)
+
+console.log("---------------------------")
+
+
+var txt = "I can eat bananas all day";
+var x = txt.slice(
+10
+, 
+16
+);
+console.log(x)
+
+var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+console.log(fruits.sort())
+;
+
+
+const Todo = [];
+Todo.push("hello")
+Todo.push("hel")
+Todo.push("helo")
+a="Hey"
+function TodoPush(){
+    Todo.push(a)
+}
+// console.log(Todo[0])
+// console.log(Todo[1])
+// for (var result in Todo){
+//     console.log(Todo)
+// }
+
+// var a = (b)=>{
+//     return b + 4
+// }
+// console.log(a(2))
+var Angular = Todo.forEach((resul)=>console.log(resul))
+console.log("------------------")
+delete Todo[0]
+// Todo.pop(Todo[0])
+TodoPush()
+Todo.forEach((resul)=>console.log(resul))
+
+arr=[]
+value='This is a push method'
+arr.push(value)
+
+console.log(arr[0])
+
+
+
